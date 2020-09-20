b0501cf39f643d564568445a27298495403ed518
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-19 22:18:36 -0700

    Create Q9.cpp

---
+++ A1/Q9.cpp
@@ -0,0 +1,2 @@
+72 = amount; this statement is invalid because amount is a variable (if declared before) and 72 is a literal and only a variable can be assigned a literal not like this.
+If the variable amount was declared, then amount = 72; would be the right statement
