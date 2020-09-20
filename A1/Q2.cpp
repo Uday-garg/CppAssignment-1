a11fb7276b5abfef8c68047ed9fdb037f1233fa7
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-19 21:48:10 -0700

    Update Q2.cpp

--- A1/Q2.cpp
+++ A1/Q2.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+int main() {
+using namespace std;
+cout << "Success\n";
+cout << "Success";
+cout << " Success\n\n";
+// It's a mad, mad program
+cout << "Success\n";
+return 0;
+}
