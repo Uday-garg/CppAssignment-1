20778d6fee6c3ca3ded4472ae81dd15c03ad8521
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-19 22:33:56 -0700

    Update Q12.cpp

--- A1/Q12.cpp
+++ A1/Q12.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 int main() {
-    int speed;
+    // declare integer variable speed
+    int speed; 
+    // store 20 in variable speed
     speed = 20;
+    // declare integer variable time
     int time;
+    // store 10 in variable time
     time = 10;
+    // declare integer variable distance
     int distance;
+    // multiple speed and time to calculate and store in the variable distance
     distance = speed * time;
+    // display calculated distance
     std::cout << distance;
 }
