3ca46b63a159cdce298705683d9be421852459a1
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-19 22:11:07 -0700

    Create Q8.cpp

---
+++ A1/Q8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int age;
+  float weight;
+  age = 26;
+  weight = 180;
+  cout << "My age is " << age << " and my weight is " << weight << " pounds." << endl;
+  return 0;
+}
