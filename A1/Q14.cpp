049c93dbb31aee784060d5f5fe6b40f787f89773
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-19 22:40:16 -0700

    Create Q14.cpp

---
+++ A1/Q14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    // declare double variable force
+    double force; 
+    // store 172.5 in variable force
+    force = 172.5;
+    // declare double variable area
+    double area;
+    // store 27.5 in variable area
+    area = 27.5;
+    // declare double variable pressure
+    double pressure;
+    // divide force by area to calculate and store in the variable pressure
+    pressure = force / area;
+    // display calculated pressure
+    std::cout << pressure;
+}
