8e9dc2071fd3406481e6419a868135f7e1dcf871
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-09-28 23:30:26 -0700

    Update and rename 32.cpp to Q32.cpp

--- A2/32.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-cout << "Here XXX represent the numerical input from user, and YYY represent the calculated numerical value by the program.";
-int selection;
-cout << "Please enter 0 to indicate USD->CAD conversion, or enter 1 to indicate CAD->USD conversion: ";
-cin >> selection;
---
+++ A2/Q32.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int selection;
+double cad, usd;
+cout << "Please enter 0 to indicate USD->CAD conversion, or enter 1 to indicate CAD->USD conversion: ";
+cin >> selection;
+switch (selection) {
+    case 1:
+      cout << "Please enter the amount of CAD dollars: ";
+      cin >> cad;
+      usd = cad / 1.32;
+      cout << "They can be converted to " << usd << " USD.";
+      break;
+    case 0:
+      cout << "Please enter the amount of USD dollars: ";
+      cin >> usd;
+      cad = 1.32 * usd;
+      cout << "They can be converted to" << cad << " CAD.";
+      break;
+    default:
+      cout << "The choices were only '0' and '1'.";
+
+}
+}
