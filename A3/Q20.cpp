6c7f6281d815061e9c7c7a8037a6e31a715a0e8b
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-10-05 23:27:28 -0700

    Update Q20.cpp

--- A3/Q20.cpp
+++ A3/Q20.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-void convert (string& c)
-{
-    for (int word = 0; word < c.length(); word++)
-        c[word] = toupper(c[word]);
-    }
 int main()
 {
-  int output, i; //i is used as an incriment
-  string c, sentence = "", k = "KPU";
-   cout << "Type the number of words your sentence is gonna have. \n\n";
-   cin >> output;
-   if (output < 0)
-    cout << "Please follow the instructions mentioned above \n\n";
-    else
-      for (i = 1; i <= output; i++)
-      {
-        cout << "Please enter the original sentence word by word without spaces: " << i << "\n";
-        cin >> c;
-        convert(c);
-        sentence = sentence + " " + c.erase(0,1) + c[0] + k;
-      }
-        cout << "Accoding to what you have entered, the suntence will be: \n " << sentence;
-   return 0;
-   }
+    string sentence, word = "", translation = "";                               
+    cout<<"Please enter the original sentence: ";                               
+    getline(cin, sentence);                                                   
+    for (unsigned int i=0; i<sentence.length(); i++) {                                     
+        if(sentence[i] == ' '){                                              
+            translation = translation + word.substr (1,string::npos)+ word[0] + "KPU ";   
+            word = "";                                                          
+        }
+        else{                                                                 
+            word = word + (char)toupper(sentence[i]);                          
+        }
+    }
+    translation = translation + word.substr (1,string::npos)+ word[0] + "KPU ";   
+    cout<<"\nTranslated: "<<translation;                                         
+    
+    return 0;
+}
