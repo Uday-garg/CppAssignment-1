f08040a90d98d8cc5a9f9738c3e6b3b38bda7a18
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-10-05 22:48:26 -0700

    Update Q20.cpp

--- A3/Q20.cpp
+++ A3/Q20.cpp
@@ -1,16 +1,42 @@
-#include <iostream>
-#include<string>
-#include <algorithm>
+// For every word in the sentence, the first letter is relocated the end of the word
+// All letters in the output are uppercase.
+// The string "KPU" is added to every word
 
+
+
+#include <iostream>
+#include <string>
 using namespace std;
 
+void convert (string& y)
+{
+    for (int a = 0; a < y.length(); a++)
+        y[a] = toupper(y[a]);
+    }
 int main()
 {
-    string s;
+  int x, i;
+  string y, sentence = "", z = "KPU";
+   cout << "Type the number of words your sentence is gonna have. \n\n";
+   cin >> x;
+   if (x <= 0)
+   {
+       cout << "please follow the instructions mentioned above \n\n";
+       }
 
-    cout << "Enter string to be reversed: ";
-    getline(std::cin, s);
-    reverse(s.begin(), s.end());
-    cout << "Resulting string is: \"" << s << "\".";
-    cout << std::endl;
-}
+   else
+   {
+       for (i = 1; i <= x; i++)
+       {
+           cout << "enter the word number " << i << "\n";
+           cin >> y;
+           convert(y);
+           sentence = sentence + " " + y.erase(0,1) + y[0] + z;
+           }
+           cout << "accoding to the question, the desired sentence is: \n " << sentence;
+       }
+   
+   
+   
+   return 0;
+   }
