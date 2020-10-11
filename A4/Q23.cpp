cfb33b3c27c65b8ce1c565b7abb031c684d2fc38
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-10-10 20:51:01 -0700

    Update Q23.cpp

--- A4/Q23.cpp
+++ A4/Q23.cpp
@@ -1,48 +1,37 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-//function prototype
-void displayLarger(int[], int, int);
-
-int main()
- {
-    //declare variables needed
-    //initialize array
-    int n, sizeof_n, arr_n;
-    cout << "What is the size of your array? "<< endl;
-    cin >> sizeof_n;
-    int arrSize = sizeof_n;
-    cout << "What are the contents of your array? (Please input numbers seperated by ',' and a space.)" << endl;
-    cin >> arr_n;
-    int numArray[arrSize] = {arr_n};
-
-    //display array for reference
-    cout << "Contents of array:\n";
-    for(int counter = 0; counter < arrSize; counter++){
-        cout << numArray[counter] << " ";
+void function(int arr_num[ ], int arr_size, int n)
+{
+    for (int i = 0; i<arr_size; i++)
+    {
+        if (arr_num[i] > n)
+        {
+           cout << arr_num[i] << " ";
+        }
     }
+    cout << endl;
+}
 
-    //call function with argument 3
-    cout << "\n\nNow calling function with argument n...\n";
-    cout << "What is the number you want as the lowest number reference to compare other numbers? ";
+int main(){
+
+    int arr_num[] = {1256,20,374,4,59,690,707,8080808,9999};
+    int arr_size = 9;
+    int n;
+    
+    cout << "The arrays are given below: \n"<<endl;
+    
+    for (int i = 0; i<arr_size; i++){
+        cout << arr_num[i]<<" ";
+        }
+        
+    cout << endl;
+
+    cout << "Please enter a number : ";
     cin >> n;
-    displayLarger(numArray, arrSize, n);
-
-
-    //return 0 to mark successful completion of program
+    cout << "The following program will give out all the numbers that are bugger than your chosen number." << endl;
+    function(arr_num,arr_size,n);  
     return 0;
- }
+}
 
- void displayLarger(int nums[], int size, int largerThan){
-    cout << "\nDisplaying all numbers in array that are ";
-    cout << "larger than " << largerThan << ":\n";
-
-    for(int counter = 0; counter < size; counter++){
-        if(nums[counter] > largerThan)
-            cout << nums[counter] << " ";
-            else 
-            cout << "There is no number bigger than the chosen number.";
-            break;
-    }
- }
