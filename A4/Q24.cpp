59dc2311ed147963529677b7aad258e72d4a48bf
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-10-10 22:57:12 -0700

    Update Q24.cpp

--- A4/Q24.cpp
+++ A4/Q24.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
+bool primeCheck(int n);
 int main() {
   int n;
   cout << "What is the number you want to test for a prime number?\n";
   cout << "Please enter a positive intiger\n";
   cin >> n;
-  if (n == 0 || n == 1){
-    cout << "False\n";
-    exit(0);
-    }
-    if (n==2) {
-    cout << "True";
-    exit(0);
-    }
-    for (int x = 2; x <= n / 2; ++x){
-      if (n % x ==0){
-      cout << "False";
-      break;}
-      else if (n%x; x!=0){
-      cout << "True";
-      break;}
-    }
+  if (primeCheck(n))
+        cout << n << " is a prime number.";
+    else
+        cout << n << " is not a prime number.";
+    return 0;
+}
+bool primeCheck(int n){
+bool prime = true;
+if (n == 0 || n == 1){
+  prime = false;
+  }
+  if (n==2) {
+  prime = true;
+  }
+  for (int x = 2; x <= n / 2; ++x){
+    if (n % x ==0){
+    prime = false;
+    break;}
+    else if (n%x; x!=0){
+    prime = true;
+    break;}
+  }
+  return prime;
 }
