a854d352664d1cfa97428384a962aead72aacc30
WadeFamer1 <71564120+WadeFamer1@users.noreply.github.com>
2020-10-25 20:31:05 -0700

    Update Q20.cpp

--- A6/Q20.cpp
+++ A6/Q20.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-class Cylinder {
-// members
-  
-};
+
+class Cylinder
+{
+private:
+    double rad; //radius
+    double hit; //height
+public:
+    
+    void setDimensions(double r, double h)
+    {
+        rad = r;
+        hit = h;
+        }
+    double getRadius() 
+    {
+        return rad;
+        }
+    double getHeight() 
+    {
+        return hit;
+        }
+    double getVolume() 
+    {
+        return 3.14159 * rad * rad * hit;
+        }    
+    
+    };
+    
+int main()
+{
+    double x, y;
+    cout << "Enter the radius of your cylinder \n";
+    cin >> x;
+    cout << "Enter the height of your cylinder \n";
+    cin >> y;
+    Cylinder volume;
+    volume.setDimensions(x, y);
+    cout << volume.getVolume();
+    }    
+    
